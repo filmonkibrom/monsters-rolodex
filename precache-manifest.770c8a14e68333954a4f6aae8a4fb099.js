1d18871f083f072623abda4e9555e1d82e503271
Filmon Kibrom <filmonkibrom@Filmons-MBP.attlocal.net>
2020-04-24 00:15:24 -0500

    Updates

---
+++ precache-manifest.770c8a14e68333954a4f6aae8a4fb099.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908eec4af4acc26dde2a17709e756aa2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3663fd88f616b58c4bfc",
+    "url": "/monsters-rolodex/static/css/main.366d09b1.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3663fd88f616b58c4bfc",
+    "url": "/monsters-rolodex/static/js/main.2d75e5ec.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
